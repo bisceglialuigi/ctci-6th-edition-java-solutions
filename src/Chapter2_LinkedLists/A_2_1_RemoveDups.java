fdaffb6a8cd72e8fad3eb128a743188e0b41fa39
Luigi Bisceglia <luigi.bisceglia94@gmail.com>
2023-11-26 19:59:17 +0100

    [CH2E2.1] Add solution

---
+++ src/Chapter2_LinkedLists/A_2_1_RemoveDups.java
@@ -0,0 +1,57 @@
+package Chapter2_LinkedLists;
+
+import Chapter2_LinkedLists.utils.LinkedList;
+import Chapter2_LinkedLists.utils.Node;
+
+import java.util.HashSet;
+
+public class A_2_1_RemoveDups {
+
+    /**
+     * Write code to remove duplicates from unsorted linked list.
+     * FOLLOW UP
+     * How would you solve this problem if a temporary buffer is not allowed?
+     */
+    public static void main(String[] args) {
+        A_2_1_RemoveDups removeDups = new A_2_1_RemoveDups();
+
+        int[][] values = new int[][]{
+                {1,2,3,4},
+                {1,1,2,3},
+                {9,8,8,8,7,6,5,3}
+        };
+
+        for(int i=0; i<values.length; i++){
+            LinkedList linkedList = new LinkedList();
+            for(int j=0; j<values[i].length; j++){
+                linkedList.insertNode(linkedList, values[i][j]);
+            }
+            linkedList = removeDups.solution(linkedList);
+            linkedList.printList();
+        }
+    }
+
+
+    /*
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+     */
+    public LinkedList solution(LinkedList linkedList){
+        Node currentNode = linkedList.getHead();
+        HashSet<Integer> intSet = new HashSet<>();
+
+        Node previousNode = currentNode;
+
+        while(currentNode != null){
+            if(intSet.contains(currentNode.data)){
+                previousNode.next = currentNode.next;
+            } else {
+                intSet.add(currentNode.data);
+                previousNode = currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return linkedList;
+    }
+}
