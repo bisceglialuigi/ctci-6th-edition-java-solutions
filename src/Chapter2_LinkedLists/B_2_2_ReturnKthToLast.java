71285f3a3b2daccb6758b0f9a19c48a37a71e563
Luigi Bisceglia <luigi.bisceglia94@gmail.com>
2023-12-07 07:34:40 +0100

    [CH2E2] Add solution

---
+++ src/Chapter2_LinkedLists/B_2_2_ReturnKthToLast.java
@@ -0,0 +1,50 @@
+package Chapter2_LinkedLists;
+
+import Chapter2_LinkedLists.utils.LinkedList;
+import Chapter2_LinkedLists.utils.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class B_2_2_ReturnKthToLast {
+    public static void main(String[] args) {
+        B_2_2_ReturnKthToLast b22ReturnKthToLast = new B_2_2_ReturnKthToLast();
+
+        LinkedList linkedList = new LinkedList();
+        linkedList.initializeFromIntValues(new Integer[]{1,2,3,4,5});
+
+        linkedList = b22ReturnKthToLast.solution(linkedList, 4);
+        linkedList.printList();
+    }
+
+
+    /*
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+     */
+    public LinkedList solution(LinkedList linkedList, int k){
+        Node currentNode = linkedList.getHead();
+
+        if(currentNode == null){
+            return linkedList;
+        }
+
+        int index = 0;
+        List<Integer> values = new ArrayList<>();
+
+        while(currentNode != null){
+            index++;
+            if(index >= k){
+                values.add(currentNode.data);
+            }
+            currentNode = currentNode.next;
+        }
+
+        LinkedList kToEndLinkedList = new LinkedList();
+        for(int i=0; i<values.size(); i++){
+            kToEndLinkedList.insertNode(values.get(i));
+        }
+
+        return kToEndLinkedList;
+    }
+}
