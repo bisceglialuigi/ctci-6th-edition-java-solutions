ac1caf3582d08d0cb2f5ebe2d3d4d37eecd86d2a
Luigi Bisceglia <luigi.bisceglia94@gmail.com>
2023-12-07 07:41:28 +0100

    [CH2E6] Add solution

---
+++ src/Chapter2_LinkedLists/F_2_6_Palindrome.java
@@ -0,0 +1,61 @@
+package Chapter2_LinkedLists;
+
+import Chapter2_LinkedLists.utils.LinkedList;
+import Chapter2_LinkedLists.utils.Node;
+
+import java.util.ArrayList;
+
+public class F_2_6_Palindrome {
+    /**
+     *  Implement a function to check if a linked list is palindrome.
+     */
+    public static void main(String[] args) {
+        F_2_6_Palindrome f26Palindrome = new F_2_6_Palindrome();
+        Integer[][] solutions = new Integer[][]{
+            new Integer[]{0,1,2,1,0},               // true
+            new Integer[]{0,1,2,2,2},               // false
+            new Integer[]{0,1,1,1,0},               // false
+            new Integer[]{0,1,1,0},                 // true
+            new Integer[]{0,1,2,2,2,3,0},           // false
+
+        };
+        LinkedList linkedList;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i=0; i<solutions.length; i++){
+            linkedList = new LinkedList();
+            linkedList.initializeFromIntValues(solutions[i]);
+            stringBuilder.append("Is boolean: ");
+            stringBuilder.append(f26Palindrome.solution(linkedList));
+            stringBuilder.append("\n");
+        }
+        System.out.print(stringBuilder);
+    }
+
+
+    /*
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+     */
+    private boolean solution(LinkedList linkedList){
+        ArrayList<Integer> list = new ArrayList<>();
+        Node currentNode = linkedList.getHead();
+
+        while(currentNode != null){
+            list.add(currentNode.data);
+            currentNode = currentNode.next;
+        }
+
+        int start = 0;
+        int end = list.size() - 1;
+
+        while(start < end){
+            if(!list.get(start).equals(list.get(end)))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
