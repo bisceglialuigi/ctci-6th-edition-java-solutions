ba5993e724cb618b84d82c9d46717344de9b1be4
Luigi Bisceglia <luigi.bisceglia94@gmail.com>
2023-12-17 19:22:25 +0100

    [CH3E5] Remove useless comments

--- src/Chapter3_StacksAndQueues/E5_SortStack/SortStack.java
+++ src/Chapter3_StacksAndQueues/E5_SortStack/SortStack.java
@@ -7,18 +7,6 @@ public class SortStack {
      *  The stack supports the following operations: push, pop, peek, isEmpty.
      */
     public static void main(String[] args) {
-        // 5 4
-        // 6 3 2 1
-        // t: 4
-
-        /*
-            while !s1.isEmpty
-                pop s1 on t
-                while s2.peek != null && t < s2.peek
-                    s1.push(s2.pop)
-                s2.push(t)
-         */
-
         Integer[] values = new Integer[]{
                 5, 6, 3, 1, 2, 4
         };
